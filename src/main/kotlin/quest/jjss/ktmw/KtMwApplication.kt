e00eace05a110a0439318c52e016762042131f37
Jaime Loeuf <jaimeloeuf@gmail.com>
2025-10-31 17:50:59 +0800

    Initial Commit:     Setup kotlin springboot project with gradle.

---
+++ src/main/kotlin/quest/jjss/ktmw/KtMwApplication.kt
@@ -0,0 +1,11 @@
+package quest.jjss.ktmw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtMwApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtMwApplication>(*args)
+}
---
+++ src/test/kotlin/quest/jjss/ktmw/KtMwApplicationTests.kt
@@ -0,0 +1,13 @@
+package quest.jjss.ktmw
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtMwApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
