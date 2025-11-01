8eef473c46754152ea89de557d7b32fa03c3f1e3
Jaime Loeuf <jaimeloeuf@gmail.com>
2025-11-02 01:02:40 +0800

    Add save method to CommandService.

--- src/main/kotlin/quest/jjss/ktmw/CommandService.kt
+++ src/main/kotlin/quest/jjss/ktmw/CommandService.kt
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service
 class CommandService(private val db: CommandRepository) {
     fun findCommands(): List<Command> = db.findAll().toList()
 
+    fun save(command: Command): Command = db.save(command)
+
     fun doesCommandExist(command: String): Boolean =
         db.findByCommandLike(command).isNotEmpty()
 
