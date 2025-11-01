c202d963400b819f646a3062a74c2ab6285f7541
Jaime Loeuf <jaimeloeuf@gmail.com>
2025-11-02 00:13:16 +0800

    Add custom query method on CommandRepository.

--- src/main/kotlin/quest/jjss/ktmw/CommandRepository.kt
+++ src/main/kotlin/quest/jjss/ktmw/CommandRepository.kt
@@ -1,5 +1,10 @@
 package quest.jjss.ktmw
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
-interface CommandRepository : CrudRepository<Command, String>
+interface CommandRepository : CrudRepository<Command, String> {
+    @Query("SELECT c FROM Command c WHERE c.command LIKE %:command%")
+    fun findByCommandLike(@Param("command") command: String): List<Command>
+}
