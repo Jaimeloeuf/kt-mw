78f1466b48be90cfd99d9ab7d78ab96171eb2e36
Jaime Loeuf <jaimeloeuf@gmail.com>
2025-11-02 01:01:59 +0800

    Use CommandService to check if command is valid.

--- src/main/kotlin/quest/jjss/ktmw/MessageService.kt
+++ src/main/kotlin/quest/jjss/ktmw/MessageService.kt
@@ -2,9 +2,14 @@ package quest.jjss.ktmw
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import org.springframework.http.HttpStatus
 
 @Service
-class MessageService(private val db: MessageRepository) {
+class MessageService(
+    private val db: MessageRepository,
+    private val commandService: CommandService
+) {
     fun findMessages(): List<Message> = db.findAll().toList()
 
     fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
@@ -22,6 +27,11 @@ class MessageService(private val db: MessageRepository) {
     fun processCommandMessage(message: Message) {
         val command = message.text.split(' ').first().substring(1)
         println("Received command: $command")
+
+        val commandExists = this.commandService.doesCommandExist(command)
+        if (!commandExists) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Command not found")
+        }
     }
 
     fun delete(id: String): Boolean {
