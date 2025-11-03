ecbc4950a757bc37aeaeed95359eab25e1698b9c
Jaime Loeuf <jaimeloeuf@gmail.com>
2025-11-03 22:29:47 +0800

    Add new Command constructor without id.

--- src/main/kotlin/quest/jjss/ktmw/Command.kt
+++ src/main/kotlin/quest/jjss/ktmw/Command.kt
@@ -2,6 +2,13 @@ package quest.jjss.ktmw
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import java.util.UUID
 
 @Table("COMMANDS")
-data class Command(@Id val id: String, val command: String, val description: String)
+data class Command(
+    @Id val id: String,
+    val command: String,
+    val description: String
+) {
+    constructor(command: String, description: String) : this(UUID.randomUUID().toString(), command, description)
+}
